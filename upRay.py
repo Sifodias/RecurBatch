094723e8d56c22b7ae08b979dcf81e3ea8ec2b38
Sifodias <coronel.ilan@gmail.com>
2021-06-09 20:58:37 +0300

    Multithreading support

--- upRay.py
+++ upRay.py
@@ -1,7 +1,15 @@
 import os
 import argparse
 import pathlib
-from multiprocessing import Process
+import multiprocessing as mp
+
+jobs = mp.JoinableQueue()
+
+def workerRoutine(jobs):
+    while True:
+        job = jobs.get()
+        os.system(job)
+        jobs.task_done()
 
 
 def preprocess_path(path, cmd=False):
@@ -18,14 +26,13 @@ def preprocess_path(path, cmd=False):
 
 
 def recDir(cmd, input):
-    # print(input)
     for obj in os.scandir(input):
         if obj.is_file():
             exec = cmd
             exec = exec.replace('#1', '\"' + obj.path + '\"')
             exec = exec.replace('#2', '\"' + os.getcwd() + '/' + obj.name + '\"')
         
-            os.system(exec)
+            jobs.put(exec)
             
 
         else:
@@ -39,10 +46,12 @@ def main():
     parser = argparse.ArgumentParser(description='Batch process files recursively')
     parser.add_argument('-i', '--input', help="The input directory")
     parser.add_argument('-o', '--output', help="The output directory")
-    parser.add_argument('cmd', help='The command you want to execute on each file. \
+    parser.add_argument('-t', '--threads', help="Number of threads to process the files", type=int, default=1)
+    parser.add_argument('cmd',  help='The command you want to execute on each file. \
                                         \nThe command should have the format \"prog [...] #1 [...] #2 [...]\" \
                                         Where #1 will be replaced by the path of the currently processed file,\n \
                                         and #2 will be replaced by its output path')
+    # type=pathlib.Path,
     args = parser.parse_args()
 
     if args.input is None or args.output is None or args.cmd is None:
@@ -53,10 +62,19 @@ def main():
     args.input = preprocess_path(args.input)
     args.output = preprocess_path(args.output)
     
+    pool = list()
+    for _ in range(args.threads):
+        worker = mp.Process(target=workerRoutine, args=(jobs,))
+        worker.start()
+        pool.append(worker)
+
     os.makedirs(args.output, exist_ok=True)
     os.chdir(args.output)
     recDir(args.cmd, args.input)
 
-
+    jobs.join()
+    for w in pool:
+        w.terminate()
+        
 if __name__=="__main__":
     main()
